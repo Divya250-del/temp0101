509d540d7ab65c898534066ab64e42b1fff3101f
Divya Jain <85102699+Divya250-del@users.noreply.github.com>
2022-04-15 08:42:06 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,33 @@
+
+from crypt import methods
+from flask import *
+app=Flask(__name__)
+
+
+@app.route('/hdfl/<n>')
+def hello(n):
+    num=list(n)
+    
+     
+    arr = list(map(int, num))
+    evens=[]
+    odds=[]
+    for i in arr:
+        if(i%2==0):
+            evens.append(i)
+        else:
+            odds.append(i)
+
+    result={
+        "number":arr,
+        "is_success": True,
+        "user_id": "divya_jain_0832IT191012",
+        "odd": odds,
+        "even": evens
+
+
+    }
+    return result
+
+if __name__== "__main__":
+    app.run(debug=True)
